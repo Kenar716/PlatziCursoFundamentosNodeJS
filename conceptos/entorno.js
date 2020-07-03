648e3fdd977946fc2424552a62f8f339e24956b1
Carlos Valdez <31641682+Kenar716@users.noreply.github.com>
2020-07-02 21:33:31 -0500

    Se agrega comentario de como asignar valores a variables de entorno en Windows según la terminal usada y los pueda detectar NodeJS.

--- conceptos/entorno.js
+++ conceptos/entorno.js
@@ -4,3 +4,16 @@ let web = process.env.SITIO_WEB || "_____________";
 console.log("Hola " + nombre);
 console.log("Mi sitio web es " + web);
 
+/*
+WINDOWS CMD Set Variables
+SET VARIABLE_NAME=variable_value
+node app_file.js
+
+SET NOMBRE=myName && node entorno.js
+
+WINDOWS Powershell Variables
+$env:VARIABLE_NAME="variable_value"
+node app_file.js
+
+$env:VARIABLE_NAME="variable_value" ; node app_file.js
+*/
